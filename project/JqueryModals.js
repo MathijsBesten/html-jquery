d0820b78c763aacdee69598be775404b215cf97c
MathijsBesten <m88_db@hotmail.com>
2016-04-06 15:40:06 +0200

    a simple concept for the project

--- project/JqueryModals.js
+++ project/JqueryModals.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    var aa = document.getElementById("aa");
+    var button = document.getElementById("butt");
+    var span = document.getElementById("close");
+    $("li").click(function () {
+        $(".proces").css({ "background-color": "gray" });
+        $("." + this.textContent).css({ "background-color": "green"});
+    });
+    $(span).click(function () {
+        modal.style.display = "none";
+    });
+});
