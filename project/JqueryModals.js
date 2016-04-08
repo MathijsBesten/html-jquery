b9170c5c29a2be759e7ffac8bf22fb8932452f68
MathijsBesten <m88_db@hotmail.com>
2016-04-08 12:58:20 +0200

    project is centered and windows with scroll usage

--- project/JqueryModals.js
+++ project/JqueryModals.js
@@ -1,13 +1,33 @@
 ﻿$(document).ready(function () {
-    var aa = document.getElementById("aa");
-    var button = document.getElementById("butt");
-    var span = document.getElementById("close");
+    var actiefA = false;
+    var actiefB = false;
+    var actiefC = false;
+    var actiefD = false;
+    var actiefE = false;
+
     $("li").click(function () {
         $(".proces").css({ "background-color": "gray" });
-        $("." + this.textContent).css({ "background-color": "green"});
+        $("." + this.textContent).css({ "background-color": "green", "z-index": "2"});
     });
-    $(span).click(function () {
-        modal.style.display = "none";
-    });
+    $("#b1, #b2, #b3").click(function () {
+        if (actiefB == false) {
+            $("#aa").fadeOut("fast");
+            actiefA = false;
+            $("#cc").fadeOut("fast");
+            actiefA = false;
+            $("#dd").fadeOut("fast");
+            actiefA = false;
+            $("#ee").fadeOut("fast");
+            actiefA = false;
 
+            $("#bb").css({ "height": "100px", "width": "250px" });
+            $("#bb-inside").css({ "height": "100px", "width": "270px" });
+            $("#bb").fadeIn("fast");
+            actiefB = true;
+        }
+        else {
+            $("#bb").fadeOut("fast");
+            actiefB = false;
+        }
+    });
 });
